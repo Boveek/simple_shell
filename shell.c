33dcd5329b05d8ffe2c068a4684222766d3c780e
Ololade Victor <oluwabori2014@gmail.com>
2023-07-26 04:33:44 -0700

    Update

--- shell.c
+++ shell.c
@@ -42,8 +42,6 @@ int main(int argc, char *argv[])
 		else
 		{
 			pipe_in = true;
-			process(argc, argv);
-			continue;
 		}
 		line_count = getline(&input_ptr, &t, stdin);
 		if (line_count == -1)
